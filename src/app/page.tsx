9be30e844cfe9c77928f6b6d8c99129f6f586cd5
prem singh <premrathore654@gmail.com>
2025-09-19 14:06:56 +0530

    ft:header added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Header from "@/components/header";
 import Test from "@/components/test";
 
 
 export default function Home() {
   return (
     <>
+    <Header />
     <Test />
     </>
   )
