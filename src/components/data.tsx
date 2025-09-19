e1d0aec0ee641351b0d14f3a7511ab71c2026577
prem singh <premrathore654@gmail.com>
2025-09-19 13:58:45 +0530

    developer branch

--- src/components/data.tsx
+++ src/components/data.tsx
@@ -4,7 +4,9 @@
 function Data() {
 
   return (
-    <div>data</div>
+    <div>data
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa quo aliquid nesciunt aliquam perspiciatis quia commodi in quas unde deleniti magnam culpa fuga nobis aspernatur, iusto maiores repudiandae, consequatur assumenda.
+    </div>
   )
 }
 
