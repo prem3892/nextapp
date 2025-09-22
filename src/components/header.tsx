0646d5b050d6dbffd89bbbe7fa0b2f9852405bf4
prem singh <premrathore654@gmail.com>
2025-09-22 13:33:48 +0530

    routing branch

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,23 +1,22 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 function Header() {
   return (
     <div>
       header
-<div className="relative w-60 h-50 bg-red-500">
-  <Image
-    src="/Slice-5_4.avif"
-    alt="logo"
-    fill
-    className="object-cover"
-    sizes='auto'
-    priority
-  />
-  </div>
-      
+      <div className="relative w-40 h-50 bg-red-500">
+        <Image
+          src="/Slice-5_4.avif"
+          alt="logo"
+          fill
+          className="object-cover"
+          sizes="auto"
+          priority
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
